64a1b026d2afe78c78c615c7a9869f3693368eff
Alberto Salas <alsalasher@gmail.com>
2023-02-13 17:57:07 +0100

    translate english

--- src/components/theme/header/index.js
+++ src/components/theme/header/index.js
@@ -7,7 +7,7 @@ const TopBar = () => {
     <header className="compTopBar navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Budget Creator</a>
         <div className="justify-content-end">
-            <button className="btn btn-outline-primary btn-sm" href="#">Perfil</button>
+            <button className="btn btn-outline-primary btn-sm" href="#">Profile</button>
             <Link className="btn btn-outline-danger btn-sm mx-3" onClick={()=>{ API.remove_token() }} to="/login">Sign out</Link>
         </div>
     </header>
