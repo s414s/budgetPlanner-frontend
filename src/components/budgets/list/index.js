9cd901b47569b464c5e7a67b8dcf7329c013ae95
Alberto Salas <alsalasher@gmail.com>
2023-02-16 09:02:33 +0100

    feature: add recycle file

--- src/components/budgets/list/index.js
+++ src/components/budgets/list/index.js
@@ -8,67 +8,87 @@ const ListBudget = () => {
 
     const [state, setState] = useState({
         status: "loading",
-        budgets: []
+        budgets: [],
+        lastModified: 1
     });
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        API.get('budgets/list/' ).then(resp => {
-            setState({...state, status: "loaded", budgets: resp.data});
+        API.get('budgets/list/').then(resp => {
+            setState({ ...state, status: "loaded", budgets: resp.data });
         });
 
-    },[]);
+    }, [state.lastModified]);
 
-    if(state.status === "loading"){
+    if (state.status === "loading") {
         return (
             <Loading text={'Cargando...'} />
         );
     };
 
+    const recycleBudget = async (idBudget) => {
+
+        try {
+            const res = await API.del('budgets/recycle/' + idBudget);
+            console.log(res)
+            if (res.status) {
+                setState({ ...state, lastModified: (+new Date()) })
+            } else {
+                alert('Budget NOT recycled')
+            }
+        } catch (error) { alert(error) }
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            timeZone: "UTC"
+        };
+
+        const lastUpdated = date.toLocaleDateString("en-US", options);
+        return `Last updated on ${lastUpdated}`;
+    };
+
     return (
         <div className="compBudgetList">
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h5 className="h2">Panel de Control</h5>
+                <h5 className="h2">My Budget List</h5>
             </div>
 
             <div className="row">
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                    <a className="nav-link active" href="#">Presupuestos</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Colaboradores</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Relaciones</a>
-                    </li>
-                </ul>
-
                 <div className="d-flex flex-column mt-3">
 
                     {state.budgets?.map(budget => {
+                        if (budget.role === 'creator') {
 
-                        return <div key={"budget"+budget.ID} className="placeholder-budget mx-3 my-2 px-5 py-4 rounded-3 shadow">
-                                    <div className="d-flex">
-                                        <div className="col-10">
-                                        <h5><Link to={"/app/decomp/folders/"+budget.ID}>{budget.title}</Link></h5>
-                                        </div>
-                                        <div className="d-flex col-2 justify-content-end">
+                            return <div key={"budget" + budget.ID} className="placeholder-budget mx-3 my-2 px-5 py-4 rounded-3 shadow">
+                                <div className="d-flex">
+                                    <div className="col-10">
+                                        <h5><Link to={"/app/decomp/folders/" + budget.ID}>{budget.title}</Link></h5>
+                                    </div>
+                                    <div className="d-flex col-2 justify-content-end">
                                         <div className="btn-toolbar mb-2 mb-md-0">
                                             <div className="btn-group me-2">
                                                 <a type="button" className="btn btn-sm btn-outline-secondary"><i className="bi bi-share"></i></a>
-                                                <a type="button" className="btn btn-sm btn-outline-secondary"><i className="bi bi-calculator"></i></a>
-                                                <a type="button" onClick={()=> { API.del("budgets/del/" + budget.ID) }} className="btn btn-sm btn-outline-secondary"><i className="bi bi-x-lg"></i></a>
+                                                <a type="button" className="btn btn-sm btn-outline-secondary"><i className="bi bi-three-dots-vertical"></i></a>
+                                                <a type="button" onClick={() => { recycleBudget(budget.ID) }} className="btn btn-sm btn-outline-secondary"><i className="bi bi-x-lg"></i></a>
                                             </div>
                                         </div>
-                                        </div>
                                     </div>
-                                    <p className="m-0 text-muted">Descripción Presupuesto {budget.ID}</p>
-                                    <p className="m-0 text-muted">Número de colaboradores</p>
-                                    <p className="m-0 text-muted">Última actualización</p>
                                 </div>
-                        }
-                    )}
+                                <p className="m-0 text-muted">{
+                                    budget.description ? budget.description : 'No description available'
+                                }</p>
+                                <p className="m-0 text-muted">{formatDate(budget.lastUpdate)}</p>
+                            </div>
+                        } else { return '' }
+                    })}
 
                 </div>
             </div>
